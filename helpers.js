58df86ec041264e41e7615f482d25aee6c2e14f5
rajkumarvoleti <voletirajkumar@gmail.com>
2022-02-22 12:29:45 +0530

    compressed image size

--- helpers.js
+++ helpers.js
@@ -20,7 +20,7 @@ exports.staticMap = ([lng, lat]) =>
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `Now That's Delicious!`;
+exports.siteName = `Dang That's Luscious!`;
 
 exports.menu = [
   { slug: "/stores", title: "Stores", icon: "store" },
